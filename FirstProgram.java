dfdcbf2452b280a0cac74a00ac1732182a3dc580
SASIKUMAR S <sskmail14@gmail.com>
2015-03-13 10:07:29 +0530

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,5 @@
 class FirstProgram{
 	public static void main(String[] param){
-		System.out.println("First Java Program in Github");
+		System.out.println("First Java Program in Github Repository");
 	}
 }
